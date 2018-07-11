bee82426f1f19241b6679ec3c80b951d2249b581
GlaDdos <kamil.kawa@gmail.com>
2018-07-11 22:57:26 +0200

    Setting up empty view in listView

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -54,6 +54,10 @@ public class CatalogActivity extends AppCompatActivity {
             }
         });
 
+        ListView petListView = (ListView) findViewById(R.id.list);
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
+
         petDbHelper = new PetDbHelper(this);
     }
 
